c8cd01ff23040eb26abdb3fc8d93a73b113d343a
Alejandro Quiroz <alejandroq1113@gmail.com>
2020-06-03 13:12:08 -0500

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Codemirror-Dummy';
+
+  codeMirrorJsonOptions: any = {
+    theme: 'idea',
+    mode: 'application/ld+json',
+    lineNumbers: true,
+    lineWrapping: true,
+    foldGutter: true,
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    lint: true
+  };
+
+  content;
+
+  ngOnInit() {
+    console.log('Iniciando la mamada esta!!!!!!!!!!!!!!!!!!!!');
+    this.content = '{ "test": true }';
+  }
 }
